e352a7337d79050f3490acf495ff978160cb7239
mugunthancloudin <mugunthan@cloudinlabs.com>
2023-11-02 18:35:15 +0530

    medichain landing page and doctor page updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Upload/>} />
+        <Route path="/" element={<LandingPage/>} />
         <Route path="/doctor" element={<DoctorPage/>}/>
       </Routes>
     </BrowserRouter>
