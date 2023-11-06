402ff7fda3050effb15dea8c168f8188a29c9668
mugunthancloudin <mugunthan@cloudinlabs.com>
2023-11-06 14:43:54 +0530

    version conflict fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Upload from "./Upload";
+import "../node_modules//bootstrap/dist/css/bootstrap.css";
 import LandingPage from "./pages/landingPage";
 import DoctorPage from "./pages/doctorPage";
 // import SuperOwnerComponent from "./components/owner/superOwnerComponent";
@@ -8,6 +7,7 @@ import OwnerPage from "./pages/ownerPage";
 
 function App() {
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
@@ -15,6 +15,7 @@ function App() {
         <Route path="/owner" element={<OwnerPage/>}/>
       </Routes>
     </BrowserRouter>
+ 
   );
 }
 
