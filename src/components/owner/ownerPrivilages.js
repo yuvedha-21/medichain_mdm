53167884cf19d19cd9c57baba2b99e3e8f4dd810
yuvedha-21 <yuvedha21@gmail.com>
2023-11-14 16:39:16 +0530

    add patient data functions

--- src/components/owner/ownerPrivilages.js
+++ src/components/owner/ownerPrivilages.js
@@ -102,6 +102,7 @@ console.log(isValidDoctor);
   //Function Call On Add owner Address
   const newOwnerAddress = async (data) => {
     try {
+      console.log(data);
       const ownerDetails = await blockchain.AddOwner(data.newOwnerAddress);
     } catch (error) {
       console.error("Error:", error);
